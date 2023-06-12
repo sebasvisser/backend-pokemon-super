a41294bcd4d607d3eb8691f31ea43c42598df229
sebasvisser <sebas.visser@mensenrelatie.nl>
2023-06-12 23:28:50 +0200

    bonus 1 abstracte klasse

--- src/Pokemon.java
+++ src/Pokemon.java
@@ -1,37 +1,32 @@
-public class Pokemon {
+public abstract class Pokemon {
     private String name;
     private int level;
 
-    // Constructor
     public Pokemon(String name, int level) {
         this.name = name;
         this.level = level;
     }
 
-    // Methods
-    public void attack() {
-        System.out.println(this.name + " is attacking!");
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public int getLevel() {
+        return level;
+    }
+
+    public void setLevel(int level) {
+        this.level = level;
     }
 
     public void defend() {
         System.out.println(this.name + " is defending!");
     }
 
-    // Getters
-    public String getName() {
-        return this.name;
-    }
-
-    public int getLevel() {
-        return this.level;
-    }
-
-    // Setters
-    public void setName(String name) {
-        this.name = name;
-    }
-
-    public void setLevel(int level) {
-        this.level = level;
-    }
+    // This is the abstract method that all subclasses must implement
+    public abstract void attack();
 }
